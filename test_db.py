3d12e50f5c571da78c62fe99f7bbde98da1615f4
Reise <147127766+Ryoung04@users.noreply.github.com>
2025-12-03 17:08:58 -0800

    Add files via upload

---
+++ test_db.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="143Rjma!", 
+    database="cs122a_project"
+)
+
+cursor = conn.cursor()
+cursor.execute("SELECT 1;")
+print(cursor.fetchone())
+
+cursor.close()
+conn.close()
+
